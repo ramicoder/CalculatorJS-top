cb74e32fe38f15c75e14a214e52335f81fa2f5cf
ramicoder <ramidaood@anonymous>
2026-02-22 17:18:35 +0300

    changed some ids into classes stored the buttons in arrays added some event listeners for the numbers and operation buttons

--- script.js
+++ script.js
@@ -5,15 +5,40 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 
-let n1;
-let n2;
+let b;
+let a;
 let operation;
 
 function operate(a, b, op) {
     return op(a, b);
 }
 
-console.log(add(1, 2));
-console.log(subtract(1, 2));
-console.log(multiply(1, 2));
-console.log(divide(1, 2));
+const log = document.getElementById("log");
+
+const nums = Array.from(document.querySelectorAll(".number"));
+
+const ops = Array.from(document.querySelectorAll(".operation"));
+
+const opts = Array.from(document.querySelectorAll(".option"));
+
+
+let currentInput = "";
+nums.map(button => 
+    button.addEventListener("click", () => {
+        currentInput += button.textContent;
+        log.value = currentInput;
+    })
+);
+
+ops.map(button =>
+    button.addEventListener("click", () => {
+
+        if (button.textContent === ".") {
+
+        } else {
+            
+            if (!a) a = Number(currentInput); // store first number
+            operation = button.textContent;    // store operator
+            currentInput = "";                 // reset for second number
+            log.value = a + " " + operation;   // show first number + operator
+        }}));
